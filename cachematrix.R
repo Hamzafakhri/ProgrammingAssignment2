fda6046ea702971876cc69f7402684dc90e171ce
Hamza Ibrahim <hamza_ghanim@homtail.com>
2021-03-12 18:36:35 +0200

    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -19,13 +19,5 @@ makeCacheMatrix <- function(input = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  inv <- x$getinverse()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data, ...)
-  x$setinverse(inv)
-  inv
+  
 }
